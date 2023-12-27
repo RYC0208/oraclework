f5bd3e221e819feddeacdf2256b829da2f6d6ef1
RYC0208 <ryc0208@naver.com>
2023-12-27 16:20:38 +0900

    Update jsp계정.sql

--- jsp계정.sql
+++ jsp계정.sql
@@ -1,39 +1,28 @@
-CREATE TABLE MEMBER (
- ID VARCHAR2(20) PRIMARY KEY,
- PWD VARCHAR2(20) NOT NULL,
- NAME VARCHAR2(20) NOT NULL,
- GENDER CHAR(1),
- BIRTHDAY CHAR(6),
- EMAIL VARCHAR2(30),
- ZIPCODE CHAR(5),
- ADDRESS VARCHAR2(100),
- DetailAddress VARCHAR2(50),
- HOBBY CHAR(5),
- JOB VARCHAR2(30)
+CREATE SEQUENCE SEQ_BOARD;
+
+CREATE TABLE board (
+	num	number PRIMARY KEY,
+	name varchar2(20) NOT NULL,
+	subject varchar2(50) NOT NULL,
+	content	varchar2(4000) NOT NULL,
+	pos	number,
+	ref	number,
+	depth number,
+	regdate	date,
+	pass varchar2(15) NOT NULL,
+	ip varchar2(15),
+	count number default 0
 );
-INSERT INTO MEMBER VALUES('kim', '1234', '홍길동', '1', '231205', 'kim@naver.com', '12345', '서울특별시 영등포구 당산동 이레빌딩', '19층','11001','학생');
-INSERT INTO MEMBER VALUES('lee', '1234', '이길동', '2', '231115', 'lee@naver.com', '23456', '인천광역시 남동구 구월동', '17층','10001','교수');
-INSERT INTO MEMBER VALUES('park', '1234', '박길동', '1', '231021', 'park@naver.com', '34567', '경기도 성남시 수정구', '수정아파트','01010','공무원');
+
+insert into board values(SEQ_BOARD.NEXTVAL, '박길동', '제목1', '내용1', 0, SEQ_BOARD.CURRVAL, 0, '2023-04-01', '1234', '0:0:0:0:0:0:0:1', default);
+insert into board values(SEQ_BOARD.NEXTVAL, '김길동', '제목2', '내용2', 0, SEQ_BOARD.CURRVAL, 0, '2023-04-05', '1234', '0:0:0:0:0:0:0:1', default);
+insert into board values(SEQ_BOARD.NEXTVAL, '송길동', '제목3', '내용3', 0, SEQ_BOARD.CURRVAL, 0, '2023-04-12', '1234', '0:0:0:0:0:0:0:1', default);
+insert into board values(SEQ_BOARD.NEXTVAL, '한길동', '제목4', '내용4', 0, SEQ_BOARD.CURRVAL, 0, '2023-04-14', '1234', '0:0:0:0:0:0:0:1', default);
+insert into board values(SEQ_BOARD.NEXTVAL, '곽길동', '제목5', '내용5', 0, SEQ_BOARD.CURRVAL, 0, '2023-04-25', '1234', '0:0:0:0:0:0:0:1', default);
+insert into board values(SEQ_BOARD.NEXTVAL, '원길동', '제목6', '내용6', 0, SEQ_BOARD.CURRVAL, 0, '2023-05-04', '1234', '0:0:0:0:0:0:0:1', default);
+insert into board values(SEQ_BOARD.NEXTVAL, '임길동', '제목7', '내용7', 0, SEQ_BOARD.CURRVAL, 0, '2023-05-07', '1234', '0:0:0:0:0:0:0:1', default);
+insert into board values(SEQ_BOARD.NEXTVAL, '신길동', '제목8', '내용8', 0, SEQ_BOARD.CURRVAL, 0, '2023-05-11', '1234', '0:0:0:0:0:0:0:1', default);
+insert into board values(SEQ_BOARD.NEXTVAL, '박길동', '제목9', '내용9', 0, SEQ_BOARD.CURRVAL, 0, '2023-05-22', '1234', '0:0:0:0:0:0:0:1', default);
+insert into board values(SEQ_BOARD.NEXTVAL, '정길동', '제목10', '내용10', 0, SEQ_BOARD.CURRVAL, 0, '2023-05-29', '1234', '0:0:0:0:0:0:0:1', default);
 
 commit;
-
-CREATE TABLE votelist (
- num number primary key,
- question varchar2(200),
- sdate date,
- edate date,
- wdate date,
- type number default 1 not null,
- active number default 1
- );
- 
- 
- CREATE TABLE voteitem (
-  listnum NUMBER,
-  itemnum NUMBER DEFAULT 0,
-  item varchar2(50),
-  count NUMBER DEFAULT 0,
-  PRIMARY KEY (listnum, itemnum)
-); 
- 
- CREATE SEQUENCE SEQ_VOTE;
